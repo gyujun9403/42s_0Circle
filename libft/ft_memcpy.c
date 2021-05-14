00290c53b4de2692ab80d380e31a80f9cd324bb5
gyujun9403 <gujun112@naver.com>
2021-05-14 23:14:33 +0900

    ing

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: gyeon <gyeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/10 17:09:29 by gyeon             #+#    #+#             */
-/*   Updated: 2021/05/14 22:12:46 by gyeon            ###   ########.fr       */
+/*   Updated: 2021/05/14 22:56:07 by gyeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memcpy(void *dst, const void *src, size_t n)
+void	*memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
 	if (dst == NULL && src == NULL)
 		;
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: gyeon <gyeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/03 16:29:42 by gyeon             #+#    #+#             */
-/*   Updated: 2021/05/14 22:12:00 by gyeon            ###   ########.fr       */
+/*   Updated: 2021/05/14 23:02:52 by gyeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,13 @@ typedef struct	s_list
 void			*ft_memset(void *b, int c, size_t len);
 void			ft_bzero(void *s, size_t n);
 void			*ft_memcpy(void *dst, const void *src, size_t n);
-void			*ft_memccpy(void * dst, const void *src, int c, size_t n);
+void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void			*ft_memmove(void *dst, const void *src, size_t len);
 void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t			ft_strlen(const char *s);
-size_t			ft_strlcat(char * dst, const char *src, size_t dstsize);
-size_t			ft_strlcpy(char * dst, const char *src, size_t dstsize);
+size_t			ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char			*ft_strchr(const char *s, int c);
 char			*ft_strrchr(const char *s, int c);
 char			*ft_strnstr(const char *haystack,
