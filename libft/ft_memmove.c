3da5f85bbb5955ecb0848a0d046e8fb337ec2198
gyujun9403 <gujun112@naver.com>
2021-05-17 22:56:14 +0900

    change func unsing ref

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: gyeon <gyeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/10 17:11:50 by gyeon             #+#    #+#             */
-/*   Updated: 2021/05/15 12:02:33 by gyeon            ###   ########.fr       */
+/*   Updated: 2021/05/17 22:53:44 by gyeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,25 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t i;
+	size_t	i;
 
-	if (dst < src && len != 0)
+	if (dst != NULL || src != NULL)
 	{
-		i = 0;
-		while (i < len)
+		if (dst < src && len != 0)
 		{
-			*((unsigned char *)dst + i) = *((unsigned char *)src + i);
-			i++;
+			i = 0;
+			while (i < len)
+			{
+				*((unsigned char *)dst + i) = *((unsigned char *)src + i);
+				i++;
+			}
+		}
+		else if (dst > src && len != 0)
+		{
+			i = len;
+			while (i-- > 0)
+				*((unsigned char *)dst + i) = *((unsigned char *)src + i);
 		}
 	}
-	else if (dst > src && len != 0)
-	{
-		i = len;
-		while (i-- > 0)
-			*((unsigned char *)dst + i) = *((unsigned char *)src + i);
-	}
-	else
-		;
 	return (dst);
 }
--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gyeon <gyeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/10 17:18:21 by gyeon             #+#    #+#             */
-/*   Updated: 2021/05/16 18:43:05 by gyeon            ###   ########.fr       */
+/*   Updated: 2021/05/17 22:51:21 by gyeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,14 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t i;
 
 	i = 0;
-	if (dst != NULL && src != NULL)
+	while (*(src + i) && (i + 1 < dstsize))
 	{
-		while (*(src + i) && (i + 1 < dstsize))
-		{
-			*(dst + i) = *(src + i);
-			i++;
-		}
-		if (dstsize > 0)
-			*(dst + i) = '\0';
-		while (*(src + i))
-			i++;
+		*(dst + i) = *(src + i);
+		i++;
 	}
+	if (dstsize > 0)
+		*(dst + i) = '\0';
+	while (*(src + i))
+		i++;
 	return (i);
 }
